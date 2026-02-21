b739df6a9ba093978758980f45ac81566b235017
LuisDev18 <brian@orbita.pe>
2026-02-21 18:21:28 -0500

    feat:first commit

---
+++ src/main/java/my/group/GreetingResource.java
@@ -0,0 +1,16 @@
+package my.group;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String hello() {
+        return "Hello from Quarkus REST pez chibolos q hacen";
+    }
+}
