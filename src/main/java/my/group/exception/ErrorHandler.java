02884f0977b423a829766122b52a8ce1b2751447
LuisDev18 <brian@orbita.pe>
2026-02-22 09:14:20 -0500

    feat: Error handler

---
+++ src/main/java/my/group/exception/ErrorHandler.java
@@ -0,0 +1,12 @@
+package my.group.exception;
+
+import java.time.LocalDateTime;
+
+public class ErrorHandler {
+
+    private LocalDateTime timestamp;
+    private String message;
+    private String path;
+    private String errorCode;
+
+}
