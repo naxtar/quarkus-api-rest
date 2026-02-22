53f09ad9a3969b68173ea9fdee752d581fa6011f
LuisDev18 <brian@orbita.pe>
2026-02-22 14:31:15 -0500

    feat:ajustes de carpeta

--- src/main/java/my/group/exception/PersonNotFoundException.java
+++ src/main/java/my/group/exception/PersonNotFoundException.java
@@ -1,7 +1,7 @@
 package my.group.exception;
 
 public class PersonNotFoundException extends RuntimeException {
-  public PersonNotFoundException(String message) {
-    super(message);
-  }
+    public PersonNotFoundException(String message) {
+        super(message);
+    }
 }
